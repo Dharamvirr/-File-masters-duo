cf225e03d785a3b95c069a47463176895e278f15
Sarvesh_Joshi <sarveshjoshi017@gmail.com>
2024-03-14 11:44:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+// configDotenv.config();
+const express = require('express');
+const app = express();
+
+const database= require("./config/db");
+const dotenv=require('dotenv')
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+database.connect();
+app.listen(PORT, ()=>
+{console.log(`Listening on port ${PORT}.`)});
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
